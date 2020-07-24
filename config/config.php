377ae8bfda5af6b444ec1a70207563fea3343368
MMBass <51884406+MMBass@users.noreply.github.com>
2020-07-24 13:34:26 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
 //DB params
-define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASS","");
-define("DB_NAME","joblister");
+define("DB_HOST","mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	");
+define("DB_USER","i1ded6cga64la3v8");
+define("DB_PASS","y7jrbju4iwoti2b0");
+define("DB_NAME","vp9pvc6a2logc4cr");
 
 define("SITE_TITLE","joblister");
