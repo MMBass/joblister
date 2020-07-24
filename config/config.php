5103c8675e9dfd798c0c988e7eb90681de06471f
MMBass <51884406+MMBass@users.noreply.github.com>
2020-07-24 13:40:14 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //DB params
-define("DB_HOST","mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	");
+define("DB_HOST","mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
 define("DB_USER","i1ded6cga64la3v8");
 define("DB_PASS","y7jrbju4iwoti2b0");
 define("DB_NAME","vp9pvc6a2logc4cr");
